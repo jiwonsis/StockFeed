31cad3bbe6bafedd160f71c0bcf04be1fa154fbf
Scott Moon <jiwonsis@naver.com>
2017-12-13 09:48:03 +0900

    Initial Commit

---
+++ StockFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockFeed
+//
+//  Created by Scott moon on 13/12/2017.
+//  Copyright © 2017 Scott moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
